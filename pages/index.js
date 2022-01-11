b92bad4aee35fba3a9fd0e055c79f124410dedcd
jjohnstondev <jakester620@gmail.com>
2022-01-10 19:16:37 -0800

    update main page

--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,14 @@ export default function Home() {
         <Header title="I am abs3nt" />
         <p>
 
-        <img src="/twitter.svg" alt="Twitter Logo" height="10px" margin="5px"/>
-          <a href="https://twitter.com/abs3ntdev"> twitter</a><br/>
-        <img src="/discord.svg" alt="Discord Logo" height="10px" margin="5px" />
-          <a href="https://discord.com/users/870047037930995722"> discord</a><br/>
-        <img src="/github.svg" alt="Github Logo" height="10px" margin="5px"/>
-          <a href="https://github.com/jjohnstondev"> github</a><br/>
-        <img src="/eth.svg" alt="Eth logo" height="10px" margin="5px"/>
-          <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> eth</a><br/>
+        <img src="/twitter.svg" alt="Twitter Logo" height="25px" margin="5px"/>
+          <a href="https://twitter.com/abs3ntdev">twitter</a><br/>
+        <img src="/discord.svg" alt="Discord Logo" height="25px" margin="5px" />
+          <a href="https://discord.com/users/870047037930995722">discord</a><br/>
+        <img src="/github.svg" alt="Github Logo" height="25px" margin="5px"/>
+          <a href="https://github.com/jjohnstondev">github</a><br/>
+        <img src="/eth.svg" alt="Eth logo" height="25px" margin="5px"/>
+          <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb">eth</a><br/>
 
         </p>
        
