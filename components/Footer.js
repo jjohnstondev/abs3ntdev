dc32d3c7e8155c65dca227e4ff5a5498ee717235
jjohnstondev <jakester620@gmail.com>
2022-01-10 17:38:00 -0800

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,13 @@
 import styles from './Footer.module.css'
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        abs3ntdev <img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} />
+        <Link href = "https://twitter.com/abs3ntdev" passHref={true}>
+          abs3ntdev <img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} />
+        </Link>
         abs3nt.eth#7465 <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
         jjohnstondev <img src="/github.svg" alt="Github Logo" className={styles.logo} />
       </footer>
