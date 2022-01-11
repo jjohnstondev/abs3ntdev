03730fe8704f063094ef4a9e85d7f6eade75e0c1
jjohnstondev <jakester620@gmail.com>
2022-01-10 18:48:57 -0800

    update main page

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,16 @@ export default function Home() {
       <main>
         <Header title="I am abs3nt" />
         <p>
+
         <img src="/twitter.svg" alt="Twitter Logo"/>
-          <a href="https://twitter.com/abs3ntdev"> abs3ntdev</a><br/>
-        {/* 
+          <a href="https://twitter.com/abs3ntdev"> twitter</a><br/>
+        <img src="/discord.svg" alt="Discord Logo" />
+          <a href="https://discord.com/users/870047037930995722"> discord</a>
+        <img src="/github.svg" alt="Github Logo" />
+          <a href="https://github.com/jjohnstondev"> github</a>
+        <img src="/eth.svg" alt="Eth logo" />
+          <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> eth</a>
 
-        <a href="https://discord.com/users/870047037930995722"> abs3nt.eth#7465</a>
-        <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
-        <a href="https://github.com/jjohnstondev"> jjohnstondev</a>
-        <img src="/github.svg" alt="Github Logo" className={styles.logo} />
-        <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> abs3nt.eth </a>
-        <img src="/eth.svg" alt="Eth logo" className={styles.logo} />
-
-        */}
         </p>
        
       </main>
