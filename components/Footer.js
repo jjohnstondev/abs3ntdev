b29d1b3a6151c4a75bde863a948ac8f259a40d9c
jjohnstondev <jakester620@gmail.com>
2022-01-10 17:31:08 -0800

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,9 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        Made with <img src="/netliheart.svg" alt="Netlify Logo" className={styles.logo} /> for you
+        abs3ntdev <img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} />
+        abs3nt.eth#7465 <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
+        jjohnstondev <img src="/github.svg" alt="Github Logo" className={styles.logo} />
       </footer>
     </>
   )
