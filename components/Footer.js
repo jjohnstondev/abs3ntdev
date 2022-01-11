df2f4fee37c5d58ae5de91ebb4e3af42db473a5f
jjohnstondev <jakester620@gmail.com>
2022-01-10 18:28:39 -0800

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,6 +10,8 @@ export default function Footer() {
         <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
         <a href="https://github.com/jjohnstondev"> jjohnstondev</a>
         <img src="/github.svg" alt="Github Logo" className={styles.logo} />
+        <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> abs3nt.eth </a>
+        <img src="/eth.svg" alt="Eth logo" className={styles.logo} />
       </footer>
     </>
   )
