f05a462eade44ff0d3797b390624aa7019f00e15
jjohnstondev <jakester620@gmail.com>
2022-01-10 18:01:16 -0800

    updated footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <footer className={styles.footer}>
         <a href="https://twitter.com/abs3ntdev"> abs3ntdev</a>
         <img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} />
-        <a href="https://discord..com"> abs3nt.eth#7465</a>
+        <a href="https://discord.com/users/870047037930995722"> abs3nt.eth#7465</a>
         <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
         <a href="https://github.com/jjohnstondev"> jjohnstondev</a>
         <img src="/github.svg" alt="Github Logo" className={styles.logo} />
