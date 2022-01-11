d12819afdb72a5def9648bbf5f4ac5e6c0ccbc37
jjohnstondev <jakester620@gmail.com>
2022-01-10 18:53:04 -0800

    update main page

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,14 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+/*
+.logo {
+  height: 1em;
+  margin: 5px;
+}
+*/
+
+
 export default function Home() {
   return (
     <div className="container">
@@ -14,14 +22,14 @@ export default function Home() {
         <Header title="I am abs3nt" />
         <p>
 
-        <img src="/twitter.svg" alt="Twitter Logo"/>
+        <img src="/twitter.svg" alt="Twitter Logo" height="1em" margin="5px"/>
           <a href="https://twitter.com/abs3ntdev"> twitter</a><br/>
-        <img src="/discord.svg" alt="Discord Logo" />
-          <a href="https://discord.com/users/870047037930995722"> discord</a>
-        <img src="/github.svg" alt="Github Logo" />
-          <a href="https://github.com/jjohnstondev"> github</a>
-        <img src="/eth.svg" alt="Eth logo" />
-          <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> eth</a>
+        <img src="/discord.svg" alt="Discord Logo" height="1em" margin="5px" />
+          <a href="https://discord.com/users/870047037930995722"> discord</a><br/>
+        <img src="/github.svg" alt="Github Logo" height="1em" margin="5px"/>
+          <a href="https://github.com/jjohnstondev"> github</a><br/>
+        <img src="/eth.svg" alt="Eth logo" height="1em" margin="5px"/>
+          <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> eth</a><br/>
 
         </p>
        
