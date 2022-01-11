f5221039e0095d88315cdbb3b5058143c093391d
jjohnstondev <jakester620@gmail.com>
2022-01-10 19:31:51 -0800

    update main page

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,13 @@ export default function Home() {
         <p>
 
         <img src="/twitter.svg" alt="Twitter Logo" height="25px" margin="5px"/>
-          <a href="https://twitter.com/abs3ntdev">twitter</a><br/>
+          <a href="https://twitter.com/abs3ntdev" target="_blank">twitter</a><br/>
         <img src="/discord.svg" alt="Discord Logo" height="25px" margin="5px" />
-          <a href="https://discord.com/users/870047037930995722">discord</a><br/>
+          <a href="https://discord.com/users/870047037930995722" target="_blank">discord</a><br/>
         <img src="/github.svg" alt="Github Logo" height="25px" margin="5px"/>
-          <a href="https://github.com/jjohnstondev">github</a><br/>
+          <a href="https://github.com/jjohnstondev" target="_blank">github</a><br/>
         <img src="/os.svg" alt="Eth logo" height="25px" margin="5px"/>
-          <a href = "https://opensea.io/abs3nt_eth">opensea</a><br/>
+          <a href = "https://opensea.io/abs3nt_eth" target="_blank">opensea</a><br/>
 
         </p>
        
