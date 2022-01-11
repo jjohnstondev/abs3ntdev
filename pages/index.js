5b9bb344251186e3063723308a551d74b2669dd1
jjohnstondev <jakester620@gmail.com>
2022-01-10 18:31:53 -0800

    updated footer

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,14 @@ export default function Home() {
       <main>
         <Header title="I am abs3nt" />
         <p className="description">
-          Welcome to my page
+        <a href="https://twitter.com/abs3ntdev"> abs3ntdev</a>
+        <img src="/twitter.svg" alt="Twitter Logo" className={styles.logo} />
+        <a href="https://discord.com/users/870047037930995722"> abs3nt.eth#7465</a>
+        <img src="/discord.svg" alt="Discord Logo" className={styles.logo} />
+        <a href="https://github.com/jjohnstondev"> jjohnstondev</a>
+        <img src="/github.svg" alt="Github Logo" className={styles.logo} />
+        <a href = "https://etherscan.io/address/0xe4306ad21a29f9edcfa9fa584e379a8d0d1463bb"> abs3nt.eth </a>
+        <img src="/eth.svg" alt="Eth logo" className={styles.logo} />
         </p>
       </main>
 
